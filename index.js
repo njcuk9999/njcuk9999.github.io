2fff8990295d6ceb16735d1f1b0776265b99ec52
Neil Cook <neil.james.cook@gmail.com>
2023-01-11 12:30:28 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+const express = require('express');
+const uploadRouter = require('./router');
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(uploadRouter);
+
+app.listen(8080, () => {
+  console.log('Form running on port 8080');
+});
